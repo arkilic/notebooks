9ceac962ac4cbcfc849991e2ccfd6e1d52c4492d
arkilic <arkilic@bnl.gov>
2016-10-06 12:17:23 -0400

    Updated amostra deployment script

--- amostra_deploy.sh
+++ amostra_deploy.sh
@@ -12,12 +12,17 @@ export HOSTNAME='xf23id-ca.cs.nsls2.local'
 
 # Get the necessary debian packages
 apt-get install sysv-rc-softioc
+
+export http_proxy=http://proxy:8888
+export https_proxy=http://proxy:8888
+
 # Create conda env
 conda create -p /opt/conda_envs/services python=3.5
 source activate services
 conda install amostra
 # conda install pymongo ujson tornado jsonschema yaml pytz doct
 # pip install requests
+
 
 # Create necessary directories for ioc
 mkdir /services
@@ -50,6 +55,11 @@ chmod -R a+r /opt/conda_envs
 
 manage-iocs install amostra
 manage-iocs enable amostra
+
+chmod 755 st.cmd
+chmod 755 launch.sh
+chmod 755 config
+
 /etc/init.d/softioc-amostra start
 
 # Then it should yield to something like below once you telnet in:
@@ -73,3 +83,25 @@ manage-iocs enable amostra
 #WARNING:tornado.access:404 GET / (10.23.0.8) 2.27ms
 #WARNING:tornado.access:404 GET /favicon.ico (10.23.0.8) 0.62ms
 #WARNING:tornado.access:404 GET /favicon.ico (10.23.0.8) 0.46ms
+
+
+
+# List of archiver machines as of 10/5/2016:
+# root@ns0:/var/lib/named/var/cache/bind# grep '\-ca[^m]' *.sub|grep -v '\-rsc\|\-cada'
+# s1030.cs.nsls2.local.sub:xf03id-ca1             A       10.3.0.5
+# s1050.cs.nsls2.local.sub:xf05id-ca1             A       10.5.0.4
+# s1100.cs.nsls2.local.sub:xf10id-ca1             A       10.10.0.4
+# s1110.cs.nsls2.local.sub:xf11id-ca              A       10.11.0.9
+# s1150.cs.nsls2.local.sub:xf11bm-ca1             A       10.11.128.4
+# s1210.cs.nsls2.local.sub:xf12id-ca1             A       10.12.0.3
+# s1230.cs.nsls2.local.sub:xf23id-ca              A       10.23.0.8
+# s1280.cs.nsls2.local.sub:xf28id-ca1             A       10.28.0.5
+# s1610.cs.nsls2.local.sub:xf16idc-ca             A       10.16.0.3
+# s1710.cs.nsls2.local.sub:xf17id1-ca1            A       10.17.0.3
+# s1720.cs.nsls2.local.sub:xf17id2-ca1            A       10.17.32.3
+# s1750.cs.nsls2.local.sub:xf17bm-ca1             CNAME   xf17bm-ioc1
+# s210.cs.nsls2.local.sub:xf02id1-ca1             A       10.2.0.4
+# s2110.cs.nsls2.local.sub:xf21id1-ca1            A       10.21.0.3
+# s410.cs.nsls2.local.sub:xf04id-ca1              A       10.4.0.3
+# s810.cs.nsls2.local.sub:xf08id-ca1              A       10.8.0.5
+# s850.cs.nsls2.local.sub:xf08bm-ca1              A       10.8.128.2
